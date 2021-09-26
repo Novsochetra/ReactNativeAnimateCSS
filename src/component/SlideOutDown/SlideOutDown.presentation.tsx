bee0cd5c4f496e927d37845904d6e6e27bff1b94
sochetra NOV <sochetra12.nov@gmail.com>
2021-09-26 19:11:57 +0700

    refactor: SlideOutDown presentation

--- src/component/SlideOutDown/SlideOutDown.presentation.tsx
+++ src/component/SlideOutDown/SlideOutDown.presentation.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {} from 'react-native';
 import Reanimated from 'react-native-reanimated';
-import { ISlideOutDownPresentationProps, Constant } from './';
+import { ISlideOutDownPresentationProps } from './';
 
 export const SlideOutDownPresentation: React.FC<ISlideOutDownPresentationProps> =
   (props) => {
