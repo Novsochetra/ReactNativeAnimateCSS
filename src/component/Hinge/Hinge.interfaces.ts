9b201985ca1b828a6a1f4f439c12e48046cb0d9f
sochetra NOV <sochetra12.nov@gmail.com>
2021-09-28 21:01:58 +0700

    add: Hinge component

---
+++ src/component/Hinge/Hinge.interfaces.ts
@@ -0,0 +1,16 @@
+import { LayoutChangeEvent, ViewStyle } from 'react-native';
+import Reanimated from 'react-native-reanimated';
+import { IEasingPreset } from '@core/animation/EasingPreset.interface';
+import { BouncPreset } from '.';
+
+export interface IHingeContainerProps {
+  delayInMS?: number;
+  durationInMS?: number;
+  preset?: keyof typeof BouncPreset;
+  finalPositionY?: number;
+}
+
+export interface IHingePresentationProps {
+  containerStyle: Reanimated.AnimatedStyleProp<ViewStyle>;
+  onLayout: (event: LayoutChangeEvent) => void;
+}
