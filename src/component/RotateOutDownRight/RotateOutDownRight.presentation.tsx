7d3c240a34a7b50b294c7df92f72932b628d8e0d
sochetra NOV <sochetra12.nov@gmail.com>
2021-10-02 23:35:11 +0700

    add: RotateOutDownRight component

---
+++ src/component/RotateOutDownRight/RotateOutDownRight.presentation.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react';
+import {} from 'react-native';
+import Reanimated from 'react-native-reanimated';
+import { IRotateOutDownRightPresentationProps } from './';
+
+export const RotateOutDownRightPresentation: React.FC<IRotateOutDownRightPresentationProps> =
+  (props) => {
+    return (
+      <Reanimated.View onLayout={props.onLayout} style={props.containerStyle}>
+        {props.children}
+      </Reanimated.View>
+    );
+  };
