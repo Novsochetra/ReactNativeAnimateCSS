d230e0f814dd1be2542534b37e97e6bcc9f9fb29
sochetra NOV <sochetra12.nov@gmail.com>
2021-10-06 17:41:26 +0700

    add: preset for ZoomIn component

--- src/component/ZoomIn/ZoomIn.container.tsx
+++ src/component/ZoomIn/ZoomIn.container.tsx
@@ -8,12 +8,14 @@ import Reanimated, {
   withDelay,
   withTiming,
 } from 'react-native-reanimated';
+import { EasingPreset } from '@core/animation/EasingPreset';
 import { Constant, IZoomInContainerProps, ZoomInPresentation } from '.';
 
 export const ZoomIn: React.FC<IZoomInContainerProps> = ({
   children,
   delayInMS = Constant.DEFAULT_DELAY_IN_MS,
   durationInMS = Constant.DEFAULT_DURATION_IN_MS,
+  preset = 'LINEAR',
 }) => {
   const opacity = useSharedValue(Constant.MIN_OPACITY);
   const scale = useSharedValue(Constant.MIN_SCALE);
@@ -32,6 +34,7 @@ export const ZoomIn: React.FC<IZoomInContainerProps> = ({
       delayInMS,
       withTiming(Constant.MAX_OPACITY, {
         duration: durationInMS,
+        easing: EasingPreset[preset],
       }),
     );
 
@@ -39,6 +42,7 @@ export const ZoomIn: React.FC<IZoomInContainerProps> = ({
       delayInMS,
       withTiming(Constant.MAX_SCALE, {
         duration: durationInMS,
+        easing: EasingPreset[preset],
       }),
     );
   }, []);
